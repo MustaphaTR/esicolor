4080c5fe467d127d3f52357aea9d5f3797f4de57
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2024-06-25 23:44:47 +0300

    Copy peace related defines from EaW.
    This may be why the AI kept some states alive as well.

--- common/defines/esicolor_defines.lua
+++ common/defines/esicolor_defines.lua
@@ -1,3 +1,6 @@
 NDefines.NGame.HANDS_OFF_START_TAG = "FIE"
 
 NDefines.NDiplomacy.WARGOAL_PER_JUSTIFY_AND_WAR_COST_FACTOR = 1.3	-- Cost factor per nation at war with or justifying against
+
+NDefines.NDiplomacy.PEACE_SCORE_DISTRIBUTION = { 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2 } -- How much of the total peace conference score you get during the first n turns.
+NDefines.NDiplomacy.PEACE_CONTEST_REFUND_FACTOR = { 1.0, 0.90, 0.80, 0.70, 0.60, 0.50 } -- How much of the spent peace conference score that gets refunded in a contest. First element applies for the first round of conflicts, second element for the second round of conflicts, etc. The final element is used for each consecutive turn, so setting that to e.g. 0.7 means you get 70 % of the spent score back for every turn thereafter.
